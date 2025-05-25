e8f905e12310610633ebc075d5f915f2888ee867
Shivek-Sharma24 <shiveksharma24@gmail.com>
2025-05-25 21:39:44 +0530

    footer reupdate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,6 @@ const Footer = () => {
   return (
     <footer className="w-full text-center md:static  bottom-0 md:mt-2  mb-2 ">
       <p className="text-gray-500">2025 Bcrypt-Generator.com</p>
-      <p className="text-sm text-gray-500 font-sans mt-1.5">
-        Built with privacy in mind. No tracking, no cookies, no data collection.
-      </p>
     </footer>
   );
 };
